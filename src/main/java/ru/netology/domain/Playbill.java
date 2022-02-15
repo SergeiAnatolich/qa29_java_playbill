7f568f2162034ced330e05aaea1837aee0767282
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-02-15 22:59:22 +0300

    corrected by comments

--- src/main/java/ru/netology/domain/Playbill.java
+++ src/main/java/ru/netology/domain/Playbill.java
@@ -2,34 +2,33 @@ package ru.netology.domain;
 
 public class Playbill {
 
-    private Movies[] movies = new Movies[0];
+    private int maxMovies = 10;
 
-    public void add(Movies movie) {
+    Movie[] movies = new Movie[0];
+
+    public Playbill() {
+    }
+
+    public Playbill(int maxMovies) {
+        this.maxMovies = maxMovies;
+    }
+
+    public void add(Movie movie) {
         int length = movies.length + 1;
-        Movies[] tmp = new Movies[length];
+        Movie[] tmp = new Movie[length];
         System.arraycopy(movies, 0, tmp, 0, movies.length);
         tmp[tmp.length - 1] = movie;
         movies = tmp;
     }
 
-    public Movies[] findAll() {
+    public Movie[] findAll() {
         return movies;
     }
 
-    public Movies[] showMaxMoviesReverseOrder(int maxMovies) {
-        Movies[] tmp = new Movies[maxMovies];
+    public Movie[] showMaxMoviesReverseOrder() {
+        Movie[] tmp = new Movie[maxMovies];
         int j = 0;
         for (int i = movies.length - 1; i > (movies.length - 1 - maxMovies); i--) {
-            tmp[j] = movies[i];
-            j++;
-        }
-        return tmp;
-    }
-
-    public Movies[] showMaxMoviesReverseOrder() {
-        Movies[] tmp = new Movies[10];
-        int j = 0;
-        for (int i = movies.length - 1; i > (movies.length - 11); i--) {
             tmp[j] = movies[i];
             j++;
         }
--- src/test/java/ru/netology/domain/PlaybillTest.java
+++ src/test/java/ru/netology/domain/PlaybillTest.java
@@ -6,29 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlaybillTest {
 
+    Movie movie1 = new Movie(1, "Бладшот", "Боевик");
+    Movie movie2 = new Movie(2, "Вперед", "Мультфильм");
+    Movie movie3 = new Movie(3, "Отель Белград", "Комедия");
+    Movie movie4 = new Movie(4, "Джентельмены", "Боевик");
+    Movie movie5 = new Movie(5, "Человек-неведимка", "Ужасы");
+    Movie movie6 = new Movie(6, "Троли. Мировой тур", "Мультфильм");
+    Movie movie7 = new Movie(7, "Номер один", "Комедия");
+    Movie movie8 = new Movie(8, "Первый встречный", "Комедия");
+    Movie movie9 = new Movie(9, "Непослушник", "Комедия");
+    Movie movie10 = new Movie(10, "Падение луны", "Боевик");
+    Movie movie11 = new Movie(11, "Яйцо Фаберже", "Комедия");
+
     @Test
     public void shouldAddOneMovie() {
-        Movies movie = new Movies(1, "Бладшот", "Боевик");
 
         Playbill playbill = new Playbill();
-        playbill.add(movie);
+        playbill.add(movie1);
 
-        Movies[] expected = {movie};
-        Movies[] actual = playbill.findAll();
+        Movie[] expected = {movie1};
+        Movie[] actual = playbill.findAll();
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldAdd() {
-        Movies movie1 = new Movies(1, "Бладшот", "Боевик");
-        Movies movie2 = new Movies(2, "Вперед", "Мультфильм");
-        Movies movie3 = new Movies(3, "Отель Белград", "Комедия");
-        Movies movie4 = new Movies(4, "Джентельмены", "Боевик");
-        Movies movie5 = new Movies(5, "Человек-неведимка", "Ужасы");
-        Movies movie6 = new Movies(6, "Троли. Мировой тур", "Мультфильм");
-        Movies movie7 = new Movies(7, "Номер один", "Комедия");
-
+    public void shouldAddSeveralMovies() {
         Playbill playbill = new Playbill();
         playbill.add(movie1);
         playbill.add(movie2);
@@ -38,27 +41,36 @@ class PlaybillTest {
         playbill.add(movie6);
         playbill.add(movie7);
 
-        Movies[] expected = {movie1, movie2, movie3, movie4, movie5, movie6, movie7};
-        Movies[] actual = playbill.findAll();
+        Movie[] expected = {movie1, movie2, movie3, movie4, movie5, movie6, movie7};
+        Movie[] actual = playbill.findAll();
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
+    public void shouldShowDefaultMaxMoviesReverseOrder() {
+        Playbill playbill = new Playbill();
+        playbill.add(movie1);
+        playbill.add(movie2);
+        playbill.add(movie3);
+        playbill.add(movie4);
+        playbill.add(movie5);
+        playbill.add(movie6);
+        playbill.add(movie7);
+        playbill.add(movie8);
+        playbill.add(movie9);
+        playbill.add(movie10);
+        playbill.add(movie11);
+
+        Movie[] expected = {movie11, movie10, movie9, movie8, movie7, movie6, movie5, movie4, movie3, movie2};
+        Movie[] actual = playbill.showMaxMoviesReverseOrder();
+
+        assertArrayEquals(expected, actual);
+    }
+
+        @Test
     public void shouldShowMaxMoviesReverseOrder() {
-        Movies movie1 = new Movies(1, "Бладшот", "Боевик");
-        Movies movie2 = new Movies(2, "Вперед", "Мультфильм");
-        Movies movie3 = new Movies(3, "Отель Белград", "Комедия");
-        Movies movie4 = new Movies(4, "Джентельмены", "Боевик");
-        Movies movie5 = new Movies(5, "Человек-неведимка", "Ужасы");
-        Movies movie6 = new Movies(6, "Троли. Мировой тур", "Мультфильм");
-        Movies movie7 = new Movies(7, "Номер один", "Комедия");
-        Movies movie8 = new Movies(8, "Первый встречный", "Комедия");
-        Movies movie9 = new Movies(9, "Непослушник", "Комедия");
-        Movies movie10 = new Movies(10, "Падение луны", "Боевик");
-        Movies movie11 = new Movies(11, "Яйцо Фаберже", "Комедия");
-
-        Playbill playbill = new Playbill();
+        Playbill playbill = new Playbill(5);
         playbill.add(movie1);
         playbill.add(movie2);
         playbill.add(movie3);
@@ -71,41 +83,8 @@ class PlaybillTest {
         playbill.add(movie10);
         playbill.add(movie11);
 
-        Movies[] expected = {movie11, movie10, movie9, movie8, movie7};
-        Movies[] actual = playbill.showMaxMoviesReverseOrder(5);
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldShowDefaultMoviesReverseOrder() {
-        Movies movie1 = new Movies(1, "Бладшот", "Боевик");
-        Movies movie2 = new Movies(2, "Вперед", "Мультфильм");
-        Movies movie3 = new Movies(3, "Отель Белград", "Комедия");
-        Movies movie4 = new Movies(4, "Джентельмены", "Боевик");
-        Movies movie5 = new Movies(5, "Человек-неведимка", "Ужасы");
-        Movies movie6 = new Movies(6, "Троли. Мировой тур", "Мультфильм");
-        Movies movie7 = new Movies(7, "Номер один", "Комедия");
-        Movies movie8 = new Movies(8, "Первый встречный", "Комедия");
-        Movies movie9 = new Movies(9, "Непослушник", "Комедия");
-        Movies movie10 = new Movies(10, "Падение луны", "Боевик");
-        Movies movie11 = new Movies(11, "Яйцо Фаберже", "Комедия");
-
-        Playbill playbill = new Playbill();
-        playbill.add(movie1);
-        playbill.add(movie2);
-        playbill.add(movie3);
-        playbill.add(movie4);
-        playbill.add(movie5);
-        playbill.add(movie6);
-        playbill.add(movie7);
-        playbill.add(movie8);
-        playbill.add(movie9);
-        playbill.add(movie10);
-        playbill.add(movie11);
-
-        Movies[] expected = {movie11, movie10, movie9, movie8, movie7, movie6, movie5, movie4, movie3, movie2};
-        Movies[] actual = playbill.showMaxMoviesReverseOrder();
+        Movie[] expected = {movie11, movie10, movie9, movie8, movie7};
+        Movie[] actual = playbill.showMaxMoviesReverseOrder();
 
         assertArrayEquals(expected, actual);
     }
